e9d67b7c3baee9fb5cb6597f43167ceb0ce02418
yazdani <arvin.yazdani1999@gmail.com>
2022-02-11 19:44:28 +0330

    create templates views and urls for start

--- books/views.py
+++ books/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def index(request):
+    return render(request, 'book/index.html')
+
+def books(request):
+    book = Book.objects.all()
+    context = {"book":book}
+    return render(request, 'book/books.html', context)
